f5f32beefa7a51a54d3b9c13b718ea5e26a4cb00
kimsjune <skim823@uwo.ca>
2023-05-05 15:33:51 -0400

    .gitignore is now working

--- innateDB.R
+++
@@ -1,7 +0,0 @@
-BiocManager::install("readxl")
-library(readxl)
-innateDB <-read_excel("innatedb_curated_genes.xls")
-innateDBviral <-dplyr::filter(as.data.frame(innateDB), grepl('virus|viral|inflamm',innateDB$Annotation))
-innateDB.gene.names <- unique(innateDB$`Gene Symbol`)
-innateDB.viral.gene.names <- unique(innateDBviral$`Gene Symbol`)
-rownames(topTags(tr.rlr.gskvdmso, n="Inf")$table) %in% innateDB.gene.names
